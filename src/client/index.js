c24202fbbeab1c5891208ef1a9b6889ec33bb8cb
Shankar RC <shanrc@cisco.com>
2018-11-22 09:40:28 +0530

    Adding Client

--- src/client/index.js
+++ src/client/index.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import Main from './main';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <Main />
+  </BrowserRouter>, document.getElementById('root')
+);
---
+++ src/client/login.js
@@ -0,0 +1,3 @@
+import React from 'react';
+const Login = () => <pre>Login</pre>;
+export default Login;
